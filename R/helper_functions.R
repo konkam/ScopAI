b44258d2d307759f01056dba8c99d1b8b958910d
konkam <guillaume.konkamking.work@gmail.com>
2021-02-20 20:27:23 +0100

    reordered the deck and gave a name for each card

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,7 +1,14 @@
+#' @importFrom magrittr "%>%"
 
-Colours = c("B", "D", "S", "C")
 
-ColoursDict = setNames(object = c("Spade", "Coppe", "Denari", "Bastoni"), nm = c("S", "C", "D", "B"))
+colours = c("B", "D", "S", "C")
+
+coloursdict = setNames(object = c("Spade", "Coppe", "Denari", "Bastoni"), nm = c("S", "C", "D", "B"))
+
+deck = expand.grid(val = 1:10, col = colours, stringsAsFactors = F) %>% (function(df) mapply(FUN = function(x,y) list(colour = x, value = y), df$col, df$val, SIMPLIFY = F))
+
+names(deck) <- paste0(rep(colours, each = 10), rep(1:10, 4))
+
 
 #' Title
 #'
@@ -12,5 +19,5 @@ ColoursDict = setNames(object = c("Spade", "Coppe", "Denari", "Bastoni"), nm = c
 #'
 #' @examples
 GiveFullColourName = function(letter) {
-  return(ColoursDict[letter])
+  return(coloursdict[letter])
 }
