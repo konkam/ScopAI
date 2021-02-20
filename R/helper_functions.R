d5ba2642a94674e5f668e001dda5a81a5b75409f
TZH <TZH@mail.com>
2021-02-20 23:08:16 +0100

    move data definition to another R script (global values)

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,14 +1,3 @@
-#' @importFrom magrittr "%>%"
-
-
-colours = c("B", "D", "S", "C")
-
-coloursdict = setNames(object = c("Spade", "Coppe", "Denari", "Bastoni"), nm = c("S", "C", "D", "B"))
-
-deck = expand.grid(val = 1:10, col = colours, stringsAsFactors = F) %>% (function(df) mapply(FUN = function(x,y) list(colour = x, value = y), df$col, df$val, SIMPLIFY = F))
-
-names(deck) <- paste0(rep(colours, each = 10), rep(1:10, 4))
-
 
 #' Title
 #'
