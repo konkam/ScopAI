eb8dfab8a8d61559aff7182276f0dd04df25a3d9
konkam <guillaume.konkamking.work@gmail.com>
2021-02-21 00:11:21 +0100

    Deal board and play card added

--- R/game_process.R
+++ R/game_process.R
@@ -28,7 +28,7 @@ RunGame = function(starting_deck, decisions1, decisions2, starting_player){
 #' @return
 #' @export
 #'
-DealCards = function(game_state, starting_player){
+DealPlayersCards = function(game_state, starting_player){
   if(starting_player!=1&&starting_player!=2){
     stop(print("starting_player should be 1 or 2"))
   }
@@ -41,5 +41,48 @@ DealCards = function(game_state, starting_player){
     game_state$hand2 = c(game_state$hand2, game_state$deck[1:3])
   }
   game_state$deck = game_state$deck[7:length(game_state$deck)]
+  return(game_state)
+}
+
+#' Title
+#'
+#' @param game_state
+#'
+#' @return
+#' @export
+#'
+#' @examples
+DealBoardCards = function(game_state){
+  game_state$board = game_state$deck[1:4]
+  game_state$deck = game_state$deck[5:length(game_state$deck)]
+  return(game_state)
+}
+
+#' Title
+#'
+#' @param game_state
+#' @param player
+#' @param decision
+#'
+#' A decision is a list(play = card played, a single card, take = cards taken.)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+PlayCard = function(game_state, player, decision){
+  if(player!=1&&player!=2){
+    stop(print("starting_player should be 1 or 2"))
+  }
+  if(player==1){
+    game_state$hand1 = game_state$hand1[game_state$hand1!=decision$play]
+    game_state$deck = game_state$deck[game_state$deck%in%decision$take]
+    game_state$stack1 = c(game_state$stack1, decision$play, decision$take)
+  }
+  if(player==2){
+    game_state$hand2 = game_state$hand2[game_state$hand2!=decision$play]
+    game_state$board = game_state$board[!game_state$board%in%decision$take]
+    game_state$stack2 = c(game_state$stack2, decision$play, decision$take)
+  }
   return(game_state)
 }
--- tests/testthat/test-game_process.R
+++ tests/testthat/test-game_process.R
@@ -1,7 +1,18 @@
 test_that("Dealing cards execute correctly", {
-  game_state = list(deck=deck, hand1 = list(), hand2 = list())
-  g = DealCards(game_state, 1)
+  game_state = list(deck=ordered_deck, hand1 = list(), hand2 = list())
+  g = DealPlayersCards(game_state, 1)
   expect_equal(length(g$hand1), 3)
   expect_equal(length(g$hand2), 3)
   expect_equal(length(g$deck), length(game_state$deck)-6)
+  g2 = DealBoardCards(g)
+  expect_equal(length(g2$board), 4)
+  expect_equal(length(g2$deck), length(g$deck)-4)
+})
+test_that("Playing cards execute correctly", {
+  game_state = list(deck=ordered_deck, hand1 = list(), hand2 = list())
+  g = DealPlayersCards(game_state, 1) %>% DealBoardCards
+  g2 = PlayCard(g, decision = list(play = "B2", take = c("B9", "B10")), player = 1)
+  expect_equal(length(g2$hand1), 2)
+  expect_equal(length(g2$hand2), 3)
+  expect_equal(length(g2$board), length(g2$board)-2)
 })
