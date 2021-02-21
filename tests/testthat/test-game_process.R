60f4bcc0d374d504740c6914628e0a6aef66edba
konkam <guillaume.konkamking.work@gmail.com>
2021-02-21 11:50:42 +0100

    fixed test

--- tests/testthat/test-game_process.R
+++ tests/testthat/test-game_process.R
@@ -27,7 +27,7 @@ test_that("Finishing game executes correctly", {
   g3 = FinishGame(game_state = g2)
   expect_equal(length(g3$board), 0)
 })
-test_that("Game runs correctly"){
+test_that("Game runs correctly",{
   game = RunGame(seed = 1, starting_player = 1, DecisionFunction = DummyDecision)
   expect_false(any(is.na(game$score_player1)))
   expect_false(any(is.na(game$score_player2)))
@@ -38,3 +38,4 @@ test_that("Game runs correctly"){
   expect_equal(unique(hands_player2 %>% lapply(length)) %>% unlist %>% sort, 0:3)
   expect_false(any(hands_player2 %>% unlist %>% is.na()))
 }
+)
