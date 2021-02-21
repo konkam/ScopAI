4c96bd2778fe7aa61652da811bd6b420403a20ea
konkam <guillaume.konkamking.work@gmail.com>
2021-02-21 11:49:05 +0100

    removed conflict markers

--- R/decision_making.R
+++ R/decision_making.R
@@ -3,11 +3,8 @@ RandomDecision <- function(game_state, player) {
   take <- c()
 }
 
-<<<<<<< HEAD
 DummyDecision = function(game_state, player){
   play = sample(x = GetPlayerHand(game_state = game_state, player = player), size = 1)
   take = c("B1")
   return(list(play = play, take = take))
 }
-=======
->>>>>>> fb3678bea0023e403fc5ddf51598cfc080719515
