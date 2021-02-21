9c78305b99531eba824be242318f6698cba6307f
konkam <guillaume.konkamking.work@gmail.com>
2021-02-21 13:32:31 +0100

    moved tests too

--- tests/testthat/test-score_functions.R
+++ tests/testthat/test-score_functions.R
@@ -1,12 +1,3 @@
-test_that("colours in a full deck", {
-  expect_equal(as.vector(GetColoursOfCards(ordered_deck)),
-               rep(colours, each = 10))
-})
-test_that("values in a full deck", {
-  expect_equal(as.vector(GetValuesOfCards(ordered_deck)),
-               rep(1:10, 4))
-})
-
 test_that("Always 10 Denari per deck", {
   expect_equal(CountDenariNumber(ordered_deck), 10)
 })
