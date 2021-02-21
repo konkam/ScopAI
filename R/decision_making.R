467283318f19e782baf96aea21cb22e30a5b94ac
konkam <guillaume.konkamking.work@gmail.com>
2021-02-21 11:46:26 +0100

    dummy decision for code development

--- R/decision_making.R
+++ R/decision_making.R
@@ -2,3 +2,9 @@ RandomDecision = function(game_state, player){
   play = sample(x = GetPlayerHand(game_state = game_state, player = player), size = 1)
   take
 }
+
+DummyDecision = function(game_state, player){
+  play = sample(x = GetPlayerHand(game_state = game_state, player = player), size = 1)
+  take = c("B1")
+  return(list(play = play, take = take))
+}
