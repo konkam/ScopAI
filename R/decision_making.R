8b60dacb6973d6f5e7dfabe6b10536e6f3a64894
konkam <guillaume.konkamking.work@gmail.com>
2021-02-22 23:56:59 +0100

    wrote an optimisable decision for testing learning algorithms

--- R/decision_making.R
+++ R/decision_making.R
@@ -11,3 +11,32 @@ DummyDecision <- function(game_state, player) {
   take <- c("B1")
   return(list(play = play, take = take))
 }
+
+invlogit = function(x) 1/(1+exp(x))
+
+OptimisableDecision <- function(game_state, player, params) {
+  hand = GetPlayerHand(game_state = game_state, player = player)
+  decisions_by_denari = list()
+  for (card in hand){
+    take_options = TakeableCardsOnBoardBruteForce(card = card, board = game_state$board)
+    for (t in take_options){
+      if (length(t)>0) denari = CountDenariNumber(c(card, t))
+      else denari = 0
+      decision = list(play = card, take = t)
+      if ( !as.character(denari) %in% names(decisions_by_denari)) decisions_by_denari[[as.character(denari)]] = list(decision)
+      else {
+        decisions_by_denari[[as.character(denari)]] = append(decisions_by_denari[[as.character(denari)]], list(decision))
+        }
+    }
+  }
+  u = runif(n = 1)
+  if (u<invlogit(params)){
+    maxdenari = max(as.numeric(names(decisions_by_denari)))
+    return(decisions_by_denari[[as.character(maxdenari)]][[1]])
+  }
+  else{
+    mindenari = min(as.numeric(names(decisions_by_denari)))
+    return(decisions_by_denari[[as.character(mindenari)]][[1]])
+  }
+
+}
--- tests/testthat/test-decision_making.R
+++ tests/testthat/test-decision_making.R
@@ -16,3 +16,22 @@ test_that("Random decision works", {
   expect_equal(unique(hands_player2 %>% lapply(length)) %>% unlist() %>% sort(), 0:3)
   expect_false(any(hands_player2 %>% unlist() %>% is.na()))
 })
+
+test_that("Optimisable decision works", {
+  set.seed(1)
+  for (i in 1:10) {
+    g <- InitialiseGameState()
+    decision <- OptimisableDecision(g, player = 1, params = 0.5)
+    expect_true(GetValueOfCard(decision$play) == GetSumValuesOfCards(decision$take) | is.null(decision$take))
+  }
+  OptimisedDecision = function(g, player) OptimisableDecision(g, player, params = 0.5)
+  game <- RunGame(seed = 1, starting_player = 1, DecisionFunction = OptimisedDecision)
+  expect_false(any(is.na(game$score_player1)))
+  expect_false(any(is.na(game$score_player2)))
+  hands_player1 <- game$game_history %>% lapply(FUN = function(x) x$player1$hand)
+  expect_equal(unique(hands_player1 %>% lapply(length)) %>% unlist() %>% sort(), 0:3)
+  expect_false(any(hands_player1 %>% unlist() %>% is.na()))
+  hands_player2 <- game$game_history %>% lapply(FUN = function(x) x$player2$hand)
+  expect_equal(unique(hands_player2 %>% lapply(length)) %>% unlist() %>% sort(), 0:3)
+  expect_false(any(hands_player2 %>% unlist() %>% is.na()))
+})
