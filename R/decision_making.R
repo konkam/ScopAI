4b7087ef105d4aef0241f8f11e124c4a1ff8ab30
theozhirsch <theo.hirsch@polytechnique.org>
2021-03-03 00:18:23 +0100

    Added 2 small functions to assess the validity of a decision

--- R/decision_making.R
+++ R/decision_making.R
@@ -1,3 +1,35 @@
+#' Is A Decision Possible
+#' Check if a decision is possible according to the decision alone, without taking account any other parameter
+#' @param decision
+#'
+IsADecisionPossible <- function(decision) {
+  if (length(decision) != 2) stop("decision has not the good format (list of length 2)")
+  if (any(sort(names(decision)) != c("play", "take"))) stop("decision has not the good format (named list with play / take)")
+  if (!"none" %in% decision$take) {
+    if (GetSumValuesOfCards(decision$take) != GetValueOfCard(decision$play)) stop("the played card value doesn't match the taken cards")
+  }
+}
+
+#' Is A Decision Valid
+#' Check if a decision is valid according to the decision alone, but also to the hand and board
+#' @param game_state
+#' @param player
+#' @param decision
+#'
+IsADecisionValid <- function(game_state = InitialiseGameState(seed = 1),
+                              player = 1,
+                              decision) {
+  IsADecisionPossible(decision)
+  if (!decision$play %in% game_state[[paste0("player", player)]]$hand) stop("the card played is not in the hand of the player")
+  if (any(!decision$take %in% c(game_state$board, "none"))) stop("some of the cards taken are not on the board")
+  if (any(GetValuesOfCards(game_state$board) == GetValueOfCard(decision$play)) &
+      length(decision$take) > 1) stop("at least one card on the board matches the value of the play, you cannot take several card")
+  if ("none" %in% decision$take) {
+    if (any(unlist(TakeableCardsOnBoardOptimized(decision$play,
+                                                 game_state$board)) != "none")) stop("you have to take cards if it is possible")
+  }
+}
+
 RandomDecision <- function(game_state, player) {
   play <- sample(x = GetPlayerHand(game_state = game_state, player = player), size = 1)
   take <- TakeableCardsOnBoardBruteForce(card = play, board = game_state$board) %>%
