30d0f4560e98fde712e2dcc83701a662b78dbd05
konkam <guillaume.konkamking.work@gmail.com>
2021-03-10 17:52:49 +0100

    now with reproduction instructions

---
+++ R/play_take_dict.R
@@ -0,0 +1,7 @@
+#' A dictionary containing, for any card, all the takeable combinations cards
+#'
+#' Using this dictionary typically entails picking a card, selecting among all the takeable combinations those that indeed appear on the table, and make a choice among these.
+#' The code to generate this dataset can be found in the data-raw/play_take_dict.R
+#'
+#' @format A named list, with the card name as the name and all allowed combinations 
+"play_take_dict"
